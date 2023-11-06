39c952a3f37cbb3da5ef89df8d5bbe7b39c2e224
Moaz2025 <moaz20ahmed@gmail.com>
2023-11-06 10:10:09 +0200

    implement main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        int n, i;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        n = scanner.nextInt();
+        int [] A = new int[n];
+        System.out.println("Enter the array's elements: ");
+        for (int k = 0; k < n; k++) {
+            A[k] = scanner.nextInt();
+        }
+        System.out.println("Enter the index of the ith element: ");
+        i = scanner.nextInt();
+        DivideAndConquer divideAndConquer = new DivideAndConquer(A);
+        int result = divideAndConquer.randomizedSelect(A,0,A.length - 1, i);
+        System.out.println(result);
+
     }
 }
