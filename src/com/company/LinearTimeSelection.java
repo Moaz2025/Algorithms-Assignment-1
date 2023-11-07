c2d92f878a4c9c223f213598cf4c1a88c34e88a0
Moaz2025 <moaz20ahmed@gmail.com>
2023-11-07 10:01:02 +0200

    some modifications

--- src/com/company/LinearTimeSelection.java
+++ src/com/company/LinearTimeSelection.java
@@ -32,11 +32,9 @@ public class LinearTimeSelection {
 
     public int getMedian(int[] A, int p, int r){
         // Sort the subarray Using insertion sort
-        int n = A.length;
         for (int i = p; i < r; i++) {
             int key = A[i];
             int j = i - 1;
-
             while (j >= 0 && A[j] > key) {
                 A[j + 1] = A[j];
                 j = j - 1;
@@ -44,42 +42,38 @@ public class LinearTimeSelection {
             A[j + 1] = key;
         }
         return A[p + (r - p) / 2];
-
     }
 
     public int select(int[] A, int p, int r, int i) {
-
         int n = r - p + 1 ;
-
         /* Divide the array into groups of size 5,
         calculate median of every group
         and store it in median[] array.*/
         int k;
-
         // There will be floor((n + 4) / 5) groups;
-        int[] median = new int[(n + 4) / 5];
+        int[] medians = new int[(n + 4) / 5];
         for (k = 0; k < n / 5; k++)
-            median[k] = getMedian(A, p + k * 5, p + k * 5 + 5);
-
+            medians[k] = getMedian(A, p + k * 5, p + k * 5 + 5);
         // For last group with less than 5 elements
         if (k * 5 < n) {
-            median[k] = getMedian(A, p + k * 5, p + k * 5 + n % 5);
+            medians[k] = getMedian(A, p + k * 5, p + k * 5 + n % 5);
             k++;
         }
-
         // Find median of all medians
-        int medOfMed = (k == 1)? median[k - 1]:
-                select(median, 0, k - 1, k / 2);
-
+        int medOfMed;
+        if(k == 1){
+            medOfMed = medians[k - 1];
+        }
+        else {
+            medOfMed = select(medians, 0, k - 1, k / 2);
+        }
         // Partition the array around the median of medians
-        int pos = partition(A, p, r, medOfMed);
-
-
-        if (pos - p == i - 1)
-            return A[pos];
-        else if (pos - p > i - 1)
-            return select(A, p, pos - 1, i);
+        int position = partition(A, p, r, medOfMed);
+        if (position - p == i - 1)
+            return A[position];
+        else if (position - p > i - 1)
+            return select(A, p, position - 1, i);
         else
-            return select(A, pos + 1, r, i - pos + p - 1);
+            return select(A, position + 1, r, i - position + p - 1);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int n, i, algorithm;
+        int n, algorithm = 1;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1 for divide and conquer, 2 for linear time selection, 3 for naive method: ");
-        algorithm = scanner.nextInt();
         System.out.println("Enter the size of the array: ");
         n = scanner.nextInt();
         int [] A = new int[n];
@@ -16,22 +14,32 @@ public class Main {
         for (int k = 0; k < n; k++) {
             A[k] = scanner.nextInt();
         }
-        System.out.println("Enter the index of the ith element: ");
-        i = scanner.nextInt();
-        if(algorithm == 1){
-            DivideAndConquer divideAndConquer = new DivideAndConquer(A);
-            int result = divideAndConquer.randomizedSelect(A, 0, A.length - 1, i);
-            System.out.println(result);
-        }
-        else if(algorithm == 2){
-            LinearTimeSelection linearTimeSelection = new LinearTimeSelection(A);
-            int result = linearTimeSelection.select(A, 0,A.length - 1, i);
-            System.out.println(result);
-        }
-        else if(algorithm == 3){
-            NaiveMethod naiveMethod = new NaiveMethod(A);
-            int result = naiveMethod.kthSmallest(A, i);
-            System.out.println(result);
+        int i = (n - 1) / 2 + 1;
+        while(algorithm != 0) {
+            System.out.println("Enter 1 for divide and conquer, 2 for linear time selection, 3 for naive method, 0 to exit: ");
+            algorithm = scanner.nextInt();
+            if (algorithm == 1) {
+                DivideAndConquer divideAndConquer = new DivideAndConquer(A);
+                long startTime = System.currentTimeMillis();
+                int result = divideAndConquer.randomizedSelect(A, 0, A.length - 1, i);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Median is: " + result);
+                System.out.println("Running time = " + (endTime - startTime) + " ms");
+            } else if (algorithm == 2) {
+                LinearTimeSelection linearTimeSelection = new LinearTimeSelection(A);
+                long startTime = System.currentTimeMillis();
+                int result = linearTimeSelection.select(A, 0, A.length - 1, i);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Median is: " + result);
+                System.out.println("Running time = " + (endTime - startTime) + " ms");
+            } else if (algorithm == 3) {
+                NaiveMethod naiveMethod = new NaiveMethod(A);
+                long startTime = System.currentTimeMillis();
+                int result = naiveMethod.kthSmallest(A, i);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Median is: " + result);
+                System.out.println("Running time = " + (endTime - startTime) + " ms");
+            }
         }
     }
 }
